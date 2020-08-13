02e5e0c2c30c78a0786beac71d752bf57d5fee31
Cristian Hornedo <officialcristianl@gmail.com>
2020-08-13 11:52:15 -0400

    Added all of the questions into the question array and started the init function by turning it into a async function to try and catch any errors

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,94 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const genMarkdown = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
-
+  "What is the name of your project?",
+  "Please enter a description of what your project is about...",
+  "How would you install this project and how would you install the dependencies (if any)?",
+  "How would this project be used?",
+  "what license are you using for this project?",
+  "What are the guidelines to contributions to be made to your project?",
+  "How do you test your project?",
+  "What license will you be using?",
+  "What is your github username?",
+  "what is your email address?",
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+// prompt the user
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "project",
+      message: questions[0],
+    },
+    {
+      type: "input",
+      name: "description",
+      message: questions[1],
+    },
+    {
+      type: "input",
+      name: "install",
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions[3],
+    },
+    {
+      type: "input",
+      name: "whatLicense",
+      message: questions[4],
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: questions[5],
+    },
+    {
+      type: "input",
+      name: "test",
+      message: questions[6],
+    },
+    {
+      type: "rawlist",
+      name: "licensebadge",
+      message: questions[7],
+      choices: ["None", "MIT"],
+    },
+    {
+      type: "input",
+      name: "username",
+      message: questions[8],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions[9],
+    },
+  ]);
 }
 
-// function to initialize program
-function init() {
+// function to write README file
+function writeToFile(fileName, data) {}
 
+// function to initialize program
+async function init() {
+  console.log("init started");
+  try {
+    const answers = await promptUser();
+
+    const readMe = genMarkdown.generateMarkdown(answers);
+
+    await writeToFile("README.md", readMe);
+    console.log("successFully wrote the README.md");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // function call to initialize program
